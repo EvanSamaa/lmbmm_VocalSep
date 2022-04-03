cbd7544964ef1a9809641b8c504c6f51229572c3
evansamaa <evan12345pan@hotmail.com>
2022-04-03 11:46:21 -0400

    e

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
-import util.dataset_util as du
+# import util.dataset_util as du
+from util.dataset_util import prepare_NUS_landmarks
 import training as tr
 import util.jali_curve_generation as jg
 import torch
@@ -34,7 +35,7 @@ if __name__ == "__main__":
     #                                "E:/MASC/lmbmm_vocal_sep_data/NUS/train_landmarks/{}.json".format(i))
     #     ja.generate_curves()
     #     print("{}.pt is completed".format(i))
-    du.prepare_NUS_landmarks()
+    prepare_NUS_landmarks()
 
     # data = NUSMusicTest("landmarks", mono=True, fixed_length=True, landmarkNoise=0.02)
     # mix, groundTruth, sideinfo = data.__getitem__(0)
